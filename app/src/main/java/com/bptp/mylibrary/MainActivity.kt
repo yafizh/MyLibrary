8fbac743177b5ebd1303e78a7e0890af666e720f
yafizh <a.chairil.yafizh@gmail.com>
2021-11-10 14:24:51 +0800

    viewpager and tablayout

--- app/src/main/java/com/bptp/mylibrary/MainActivity.kt
+++ app/src/main/java/com/bptp/mylibrary/MainActivity.kt
@@ -2,28 +2,44 @@ package com.bptp.mylibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import com.bptp.mylibrary.databinding.ActivityMainBinding
-import com.mancj.materialsearchbar.MaterialSearchBar
+import androidx.viewpager2.widget.ViewPager2
+import com.bptp.mylibrary.ui.category.CategoryFragment
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private fun setupViewPager() {
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewPagerAdapter.addFragment(HomeFragment.newInstance(), "Undone Task")
+        viewPagerAdapter.addFragment(NewCollectionFragment.newInstance(), "aaa Task")
+        viewPagerAdapter.addFragment(CategoryFragment.newInstance(), "Undone Task")
+        binding.viewpager2.adapter = viewPagerAdapter
+        binding.viewpager2.isUserInputEnabled = false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         supportActionBar?.hide()
+        setupViewPager()
 
-        binding.searchBar.setSpeechMode(true)
-    }
 
-    override fun onClick(view: View?) {
-        when(view?.id){
-            MaterialSearchBar.BUTTON_SPEECH->{
-                Log.d("LKjjjjj", "onClick: aaa")
+        binding.bubbleTabBar.addBubbleListener { id ->
+            when (id) {
+                R.id.home -> {
+                    binding.viewpager2.currentItem = 0
+                }
+                R.id.news -> {
+                    binding.viewpager2.currentItem = 1
+                }
+                R.id.category -> {
+                    binding.viewpager2.currentItem = 2
+                }
             }
         }
+
     }
+
 }
